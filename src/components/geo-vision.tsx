78f7b1a897e74f576092f668da8cc8277f7c19a1
Sudipta <sudipta.chanda@sakariya.in>
2025-09-16 11:05:54 +0530

    commit with working code

--- src/components/geo-vision.tsx
+++ src/components/geo-vision.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:179a690359a0c61edc503b1ec3e7e8bf765ed6add3096aac2f19f511ba7acb3a
-size 36550
+oid sha256:37316f2c42ebfcc01388562091232a5841998a633c12f2261fdc3d55d198fbad
+size 36599
