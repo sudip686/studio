fecf3a98e3c910a409cfd18829ed5d9933f48eb1
Sudipta <sudipta.chanda@sakariya.in>
2025-12-19 13:59:23 +0530

    issue with vulnerabilities

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:149e2b3ee5a79048b329b089ab435a3b3cea9e33c78cf97b2e7304880a0475e7
-size 3477
+oid sha256:020e9a0d1b5527949f8d2f2281852df1147027d41d7fcad875e8ba2c9a937c45
+size 4372
