f9d99890b877eaf54d0f64b5258a25ab3aeb9467
Sudipta <sudipta.chanda@sakariya.in>
2025-12-19 13:57:48 +0530

    fix vulnerabilities

--- workbox.config.js
+++ workbox.config.js
@@ -7,8 +7,14 @@ module.exports = {
   globPatterns: [
     // App shell + static assets you want precached:
     '**/*.{js,css,html,ico,png,svg,webp,woff2}',
-    // If you ship fixed base data in public/data/, precache a minimal set too:
-    'data/**/*.{geojson,kml,kmz}'
+    // Geospatial data files (geojson, kml, kmz) - in public root
+    '*.{geojson,json,kml,kmz}',
+    // Model and other data files
+    '*.{glb,bin,tiff,jpg}'
+  ],
+  globIgnores: [
+    '**/node_modules/**/*',
+    'service-worker.js'
   ],
   maximumFileSizeToCacheInBytes: 15 * 1024 * 1024 // allow large GeoJSON/KMZ if needed
 };
