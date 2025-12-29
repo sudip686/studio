fadfb4e39376293b2782e5b173d2c99d815ee7b8
Sudipta <sudipta.chanda@sakariya.in>
2025-12-29 11:21:43 +0530

    fixed assay view

--- src/components/DrillholeLayer.tsx
+++ src/components/DrillholeLayer.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff845f96296754f30d442ff4ef6cee36dfa2f31cfdceeacdcaf111ecf8e0bd7a
-size 10122
+oid sha256:4e42d964c216679d9ee9c1eb29605de19b64fa2c4d279ea28167929034322bb2
+size 10551
