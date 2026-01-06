08c22fa1020edd48086f3395d6e1dd1fa383492e
Sudipta <sudipta.chanda@sakariya.in>
2026-01-06 14:46:36 +0530

    fixed few more layout uissue

--- src/components/viewers/BlockModelRescView.tsx
+++ src/components/viewers/BlockModelRescView.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aed5f8ccf90144825b11c2897e8222c7a4033e878eeaeb20eb89521d71bf3909
-size 13764
+oid sha256:3bae23f21615331cab1b4d802b4dc2292f79d10866d5788dd09c420776c33188
+size 14947
