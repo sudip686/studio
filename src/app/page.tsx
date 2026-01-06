240a5319fed1881b8a044174b837df8d2237f690
Sudipta <sudipta.chanda@sakariya.in>
2026-01-06 15:02:40 +0530

    fixed some filtering issue in block model view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:153339ace879a41c69f17086b9ddaaaf0579739bb84501b7d24318f141335847
-size 7033
+oid sha256:b20da9289358a524151cb4a78c1a66eabb95dc1f9b1d4f78cadffaf9055fec93
+size 7036
