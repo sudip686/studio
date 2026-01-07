515d10bf29bc975195deb9347b504e0e37aa172c
Sudipta <sudipta.chanda@sakariya.in>
2026-01-07 12:30:04 +0530

    compass

--- src/components/ui/MetricScaleOverlay.tsx
+++ src/components/ui/MetricScaleOverlay.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dd5ed68972172beee675a9df5f780bead198b1407ca39d84664cf1bac2344bdf
-size 1829
+oid sha256:4d746efb73c8c0c7e9e75b16c43858cc38c2c7a19390094c4a3a32ede111b6af
+size 2907
--- src/contexts/cesium-context.tsx
+++ src/contexts/cesium-context.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:63dfa054e8edcbdcf83b73a52332b5aa0284db490a9164eebcfa9de27c185cc5
-size 8164
+oid sha256:6dcf86004a12bcd700914aa03e0c0af63bed01490ada41abddaf238b1bde4d43
+size 8516
