821643ddd88e2dee7a61a376e7aed70d13655b58
sudichan <sudipta.chanda@oracle.com>
2026-01-20 20:58:11 +0530

    added undercround view

--- src/contexts/cesium-context.tsx
+++ src/contexts/cesium-context.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bc265a8041e03d859b596d8ca407ecfbaf9a76d52b528d6f333b26bccacaf90e
-size 15931
+oid sha256:9fb60e7cfdf4962aa15a8171c5272130bdd44eab1d1f6bb377ff69e80962d0bb
+size 18612
