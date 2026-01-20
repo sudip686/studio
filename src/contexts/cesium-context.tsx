6b21458152a857a1602db9963c7febb320719b23
sudichan <sudipta.chanda@oracle.com>
2026-01-20 20:22:07 +0530

    fixed cesium view change

--- src/contexts/cesium-context.tsx
+++ src/contexts/cesium-context.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dd2921839e237615ed36b03182cf639229e95caf770f9b3e6ee1e52abe3a4e5b
-size 10093
+oid sha256:69fee23853144dcdbd07274e3606d4facdd858ef88b5256816c7b775d097ab85
+size 14062
