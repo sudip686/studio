00b2e9f33b3ff02e814671f28099327ebd52980e
sudichan <sudipta.chanda@oracle.com>
2026-01-20 23:35:27 +0530

    fixed layout issue

--- src/components/shared/GlobalOverlays.tsx
+++ src/components/shared/GlobalOverlays.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b21f0b6528f24f39f4dcb404e2252a7cdc99fe621dc2c31f13bafe7871ae88d6
-size 6581
+oid sha256:a76694d1afb7d7875a65cc7772453ff8f8ea53960874ed60906101bdbce52b4b
+size 7941
