3768d6b71570d60b838ea728360d35eeaf4d5e5b
sudichan <sudipta.chanda@oracle.com>
2026-01-20 21:59:04 +0530

    added few changes

--- src/contexts/cesium-context.tsx
+++ src/contexts/cesium-context.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dd2921839e237615ed36b03182cf639229e95caf770f9b3e6ee1e52abe3a4e5b
-size 10093
+oid sha256:d02d5fb777291627fb1a03b0ced8ec6aaacc665a50ee7861109439ac07c2967f
+size 12901
