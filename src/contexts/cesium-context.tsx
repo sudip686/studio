87cd62c30fed0b2f3c7647655a80cac3669b9467
sudichan <sudipta.chanda@oracle.com>
2026-01-20 21:29:39 +0530

    added new views , movement better

--- src/contexts/cesium-context.tsx
+++ src/contexts/cesium-context.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9fb60e7cfdf4962aa15a8171c5272130bdd44eab1d1f6bb377ff69e80962d0bb
-size 18612
+oid sha256:58d81405bd2da3af1b158ce46c847e9b4f8f9de5c172bb7d590ccac2e922de3f
+size 26406
