7ea63f222bf70cd15753fcef90b5c9aacbac57f0
sudichan <sudipta.chanda@oracle.com>
2026-01-20 20:22:07 +0530

    fixed cesium view change

--- src/components/CesiumViewSwitch.tsx
+++ src/components/CesiumViewSwitch.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cee42476c96233b9d1dffcf2eb0e1f7aa6a98958a5a133a7cb69676573fc9b13
-size 18809
+oid sha256:8a8ce7aea03d112b6b81788975b148043b4f47deb299cdaf88182bc91aed766d
+size 18202
