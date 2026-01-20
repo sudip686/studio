36e4bed2e81eec7439914f8a263ec1946483a3b0
Sudipta <sudipta.chanda@sakariya.in>
2026-01-20 12:17:29 +0530

    added new feature

--- src/components/ImmersivePresentationViewer.tsx
+++ src/components/ImmersivePresentationViewer.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bb68cd54b818dbf2165a14d8bb2da3a33d36f1a56b80a01e55ce912161a5a9c1
-size 21128
+oid sha256:8417d7ce471d5f3d595edf5f72d04c5af9f634ff8ee9634eeb1d877a21f0c933
+size 21642
