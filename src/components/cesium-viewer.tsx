d002c6b997a39435b1859ebe2438cd8efdd2cef2
Sudipta <sudipta.chanda@sakariya.in>
2026-01-29 11:13:30 +0530

    added some faster code

--- src/components/cesium-viewer.tsx
+++ src/components/cesium-viewer.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ca18747d2147bb8fb0387754a4264561f03cc652d5653a2d2bbcab6ced0be9df
-size 23225
+oid sha256:2bdbccd9cd15b827fad45793ec0ecbcce2910e7e1c49994446a71138ae36fa9f
+size 23716
