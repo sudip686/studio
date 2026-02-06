a51837a202367fe42ffa7f1724af822240454e1f
Sudipta <sudipta.chanda@sakariya.in>
2026-02-06 13:31:30 +0530

    feat(subsurface): Implement BlockModelLayer with InstancedMesh rendering

---
+++ src/components/__tests__/BlockModelLayer.test.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9b18c6e54b432b1ec8de7f36a0d3c2ceb4e589aca3c2b56624d729a584206a39
+size 2502
---
+++ src/components/viewers/BlockModelLayer.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f66e6753cc0df42db81227440e12e675e2686f9b2e8e29720e7a8750b1329690
+size 4591
