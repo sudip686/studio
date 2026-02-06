414265672550c4016111ba5685576ebab2791ff0
Sudipta <sudipta.chanda@sakariya.in>
2026-02-06 13:36:17 +0530

    feat(subsurface): Implement BoreholeLayer using InstancedMesh

---
+++ src/components/__tests__/BoreholeLayer.test.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:29be2798b8a7fd9e04e309628c1bd1fa9529a4e998b30f0b6f45fcee76104e35
+size 2694
---
+++ src/components/viewers/BoreholeLayer.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6e8e9f6aaff90b4350e89cd4ff3976da198eeb0ef9cbb42a584d2a0addc6a675
+size 5427
