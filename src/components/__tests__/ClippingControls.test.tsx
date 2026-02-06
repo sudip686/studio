b4b4af53d72268a328a293fd25ef1e0c0d79aa50
Sudipta <sudipta.chanda@sakariya.in>
2026-02-06 13:39:33 +0530

    feat(subsurface): Implement ClippingControls UI

---
+++ src/components/__tests__/ClippingControls.test.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3315d3e93d32072bba60d4fb6995027108bb53ccdcea18365491e12b38c4aee9
+size 1986
---
+++ src/components/viewers/ClippingControls.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:111151112164c1109173ff19a6a5c39ecb01efd2f2477472cdfbf7b8afe01be4
+size 2847
