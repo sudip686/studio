7d713348d22cc90d3dfe04c515ad548fb29db72d
Sudipta <sudipta.chanda@sakariya.in>
2026-02-06 11:28:01 +0530

    feat(subsurface): Extract common Cesium/Three.js setup logic into useGeoScene hook

---
+++ src/hooks/__tests__/useGeoScene.test.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c28f9192da9b87a0e3bf069f038d8fea79701803c8ffcc380cbb25dbe17e97ff
+size 1842
---
+++ src/hooks/useGeoScene.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:89eec74a43c47a7cc8a9b60aabd89f509a049825ae199e98281e33d690d724bc
+size 3900
