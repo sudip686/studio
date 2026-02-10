c92a13d93fa73ed112cf7dea005d4b0eb72eb375
Sudipta <sudipta.chanda@sakariya.in>
2026-02-10 17:13:26 +0530

    feat(ui): remove immersive presentation and enforce navigation boundaries

---
+++ src/app/__tests__/page.test.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:63a4a0f25aec10f79939dd75a9c57867256eb658a40fd34a339135f6ecee61e9
+size 2086
--- src/components/ImmersivePresentationViewer.tsx
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:9a8277cbac6eb5eb45775f1ef4d304d6e9fec831ece39dcaa08f7e027532427e
-size 22187
