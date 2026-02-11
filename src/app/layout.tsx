0a46e75d44608e5e435949f224c2a4cc32467297
Sudipta <sudipta.chanda@sakariya.in>
2026-02-11 14:50:36 +0530

    chore(perf): Update layout preload to height.bin

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5fc8f8885110c524f2d95b5fd765ef8455d0a29f5250e8c4f9ed579260bf3e2f
-size 1818
+oid sha256:e26898e9daa9dc79a486c88aebd75315a5ec9600eb1eb3f49056021acca719ea
+size 1740
