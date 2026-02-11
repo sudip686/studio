a647a39621af27001937a89ae1d4e2c5b5113597
Sudipta <sudipta.chanda@sakariya.in>
2026-02-11 17:27:15 +0530

    fix: Final removal of alpha channel from hex colors in legend-definitions.ts
    Corrected the last remaining hex color with an alpha channel ('#4c54549c' to '#4c5454') in geospatialViewerLithologyLegendData to ensure full compatibility with THREE.Color and prevent warnings.

--- src/lib/constants/legend-definitions.ts
+++ src/lib/constants/legend-definitions.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:90603573cb645e331e6a5e6a3419ba4cb1a3037aea4410bd5966393eae684d0b
-size 4641
+oid sha256:7605982c7adca227ca77be688dc992333de87952381ea6369cfd4fcaf3db83ef
+size 4639
